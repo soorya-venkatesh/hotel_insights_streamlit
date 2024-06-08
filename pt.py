91065bddc26f6a5da7f61bc3c26695186ce96f3d
soorya-venkatesh <soorygethu@gmail.com>
2024-06-08 05:08:48 -0700

    update username and email in yaml file

--- pt.py
+++ pt.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import random
-df=pd.read_csv('test.csv')
+df=pd.read_csv('./test.csv')
 df.loc[len(df.index)] = ['ahem',random.random()]
 df.to_csv('test.csv',index=False)
 print(df)
+
