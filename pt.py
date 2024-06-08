8c8a65b41d5a4828a4e39249bca6cad1da9288b2
soorya-venkatesh <soorygethu@gmail.com>
2024-06-08 05:01:18 -0700

    test cron to verify

--- pt.py
+++ pt.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import random
 df=pd.read_csv('test.csv')
-
-st.write(df)
+df.loc[len(df.index)] = ['ahem',random.random()]
+df.to_csv('test.csv',index=False)
+print(df)
